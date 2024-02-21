4fa8a7ad7781121dc1126452c80b211343607301
Albert Wu <albertwujj@gmail.com>
2024-02-20 23:25:25 -0800

    change name and description of claw

---
+++ src/main/java/caclawmod/ClawNamePatch.java
@@ -0,0 +1,17 @@
+package caclawmod;
+
+import com.evacipated.cardcrawl.modthespire.lib.SpireInsertPatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePrefixPatch;
+import com.megacrit.cardcrawl.cards.blue.Claw;
+import com.megacrit.cardcrawl.localization.CardStrings;
+
+@SpirePatch(clz=Claw.class, method=SpirePatch.CONSTRUCTOR)
+public class ClawNamePatch {
+    @SpireInsertPatch(rloc=1)
+    public static void Insert(Claw __instance) {
+        __instance.name = "Ca-claw!";
+        __instance.rawDescription = "Deal !D! damage. NL Add a copy of this card to your discard pile. Increase the damage of ALL Claw cards by !M! this combat.";
+        __instance.initializeDescription();
+    }
+}
